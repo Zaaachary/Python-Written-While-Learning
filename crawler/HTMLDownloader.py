6553d4d280d7b36cc5088ad94a02600695632429
Zachary Lee <li.zaaachary@gmail.com>
2018-08-22 21:20:48 +0800

    开始制作股票数据定向爬虫

--- crawler/HTMLDownloader.py
+++ crawler/HTMLDownloader.py
@@ -1,5 +1,8 @@
+# coding=utf-8
 # 一个下载网页的程序
 import requests as rqs
+import re
+import os
 
 
 def get_html(url):
@@ -7,13 +10,39 @@ def get_html(url):
         r = rqs.get(url, timeout=30)
         r.raise_for_status()
         r.encoding = r.apparent_encoding
+        return r.text, r.content
     except Exception as e:
+        return "none", "html 获取失败"
 
 
-def save_html(path):
+def get_title(text):
+    pattern = r'<title>.*</title>'
+    try:
+        m = re.search(pattern, text)
+        title = m.group(0).split("<title>")[1].split("</title>")[0]
+    except Exception as e:
+        title = 'TitleNoFound'
 
+    return title
+
+
+def save_html(title , content):
+    root = 'webpage/'
+    path = root + '/' + title + '.html'
+    if not os.path.exists(root):
+        os.mkdir(root)
+    if not os.path.exists(path):
+        with open(path, 'wb') as f:
+            f.write(content)
+            print("文件保存成功")
+    else:
+        print("文件已经存在")
 
 
 if __name__ == '__main__':
     url = input("请输入要下载的网址")
-    path = ''
+    # url = "http://www.baidu.com"
+    text, content = get_html(url)
+    title = get_title(text)
+    save_html(title, content)
+
--- crawler/stockcrawler.py
+++ crawler/stockcrawler.py
@@ -10,18 +10,61 @@ import re
 
 
 def get_html_text(url):
-    return ""
+    try:
+        r = requests.get(url, timeout=30)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return ""
 
 
 def get_stock_list(lst, stockURL):
+    html = get_html_text(stockURL)
+    soup = BeautifulSoup(html, 'html.parser')
+    a = soup.find_all('a')
+    for i in a:
+        try:
+            href = i.attrs['href']
+            lst.append(re.findall(r"[s][hz]\d{6}", href)[0])
+        except:
+            continue
+
+
     return ""
 
 
 def get_stock_info(lst, stockURL, fpath):
-    return ""
+    for stock in lst:
+        url = stockURL + stock + ".html"
+        html = get_html_text(url)
+        try:
+            if html == "":
+                continue
+            infoDict = {}
+            soup = BeautifulSoup(html, 'html.parser')
+            stockInfo = soup.find('div', attrs={'class': 'stock-bets'})
+            name = stockInfo.find_all(attrs={'class': 'bets-name'})[0]
+            infoDict.update({'股票名称': name.text.split()[0]})
+
+            keyList = stockInfo.find_all('dt')
+            valueList = stockInfo.find_all('dd')
+            for i in range(len(keyList)):
+                key = keyList[i].text
+                val = valueList[i].text
+                infoDict[key] = val
+
+            with open(fpath, 'a', encoding='utf-8') as f:
+                f.write(str(infoDict) + '\n')
+        except:
+            traceback.print_exc()
+            continue
 
 
 if __name__ == '__main__':
     stock_list_url = 'http://quote.eastmoney.com/stocklist.html'
     stock_info_url = 'https://gupiao.baidu.com/stock/'
-    output_path = ''
+    output_path = 'BaiduStockInfo.txt'
+    slist = []
+    get_stock_list(slist, stock_list_url)
+    get_stock_info(slist, stock_info_url, output_path)
