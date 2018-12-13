fa60c36f33ad42fa670fec39ca64c29a39f38def
Zachary Lee <Li.zaaachary@gmail.com>
2018-12-13 16:11:38 +0800

    fix confilct

--- opencv/03_edge.py
+++ opencv/03_edge.py
@@ -2,11 +2,11 @@
 
 import cv2
 
-image = cv2.imread('images/classr.jpg')
-cv2.namedWindow("Image1")
+image = cv2.imread('images/1.jpg')
+# cv2.namedWindow("Image1")
 cv2.namedWindow("Image2")
 
-cv2.imshow("Image1", image)
+# cv2.imshow("Image1", image)
 
 
 # Canny函数，三个参数：源图像，低阈值，高阈值
